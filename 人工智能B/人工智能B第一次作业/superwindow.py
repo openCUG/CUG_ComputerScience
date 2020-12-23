5fd2585e51cf5afbc677ae5de8bde09dca8c393d
ChangWenhan <53159178+ChangWenhan@users.noreply.github.com>
2020-12-23 23:20:43 +0800

    Add files via upload

--- 人工智能B/人工智能B第一次作业/superwindow.py
+++ 人工智能B/人工智能B第一次作业/superwindow.py
@@ -5,23 +5,41 @@ from PyQt5.QtCore import *
 from paint import PaintWidget   #含有画布
 from dfsClass import *  #含有链表类
 from bfsClass import *  #含有节点和图类
+import numpy as np
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.initialBox()
+        #按键的槽函数
+        self.dfsButton.clicked.connect(self.dfsDeal)
+        self.bfsButton.clicked.connect(self.bfsDeal)
+        self.greedyButton.clicked.connect(self.greedyTest)
+        self.clearButton.clicked.connect(self.clearChecked)
+        #图的数据以及节点的标号
+        self.readyPlaceHolder()
+        # 为深度遍历进行准备工作
+        self.dfsPlaceHolder()
+        # 为广度遍历进行准备工作
+        self.bfsPlaceholder()
+        #为贪婪算法进行准备工作
+        self.greedyPlaceHolder()
+        # 添加画布以及绘画事件
+        self.painterPlaceHolder()
+        self.show()
+
+    #对话框的初始化规格
+    def initialBox(self):
         self.title = 'PyQt paint'
         self.width = 1090
         self.height = 680
         self.count = 0
         self.setWindowTitle(self.title)
         self.setGeometry(0, 0, self.width, self.height)
-        #按键的槽函数
-        self.dfsButton.clicked.connect(self.dfsDeal)
-        #self.bfsButton.clicked.connect(self.test)
-        self.bfsButton.clicked.connect(self.bfsDeal)
-        self.clearButton.clicked.connect(self.clearChecked)
-        #图的数据以及节点的标号
+
+    #设置节点标号与权重的对应字典，以及连接关系
+    def readyPlaceHolder(self):
         self.dict = {1: self.oradea, 2: self.zerind, 3: self.arad,
                      4: self.timisoara, 5: self.lugoj, 6: self.mehadia,
                      7: self.dobreta, 8: self.craiova, 9: self.pitesti,
@@ -34,16 +52,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                      [8, 13], [13, 8], [9, 10], [10, 9],
                      [9, 13], [13, 9], [10, 11], [11, 10],
                      [11, 12], [12, 11], [12, 13], [13, 12]]
-        # 为深度遍历进行准备工作
-        self.dfsPlaceHolder()
-        # 为广度遍历进行准备工作
-        self.bfsPlaceholder()
-        # 添加画布以及绘画事件
+
+    #为画布做准备
+    def painterPlaceHolder(self):
         self.m = PaintWidget(self)
         self.m.move(0, 0)
         self.m.resize(self.width, self.height)
         self.resize(1090, self.height + 100)
-        self.show()
 
     #用于清除画面中的选中状态
     def clearChecked(self):
@@ -156,3 +171,68 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         print('深度优先遍历的顶点：')  # 打印深度优先遍历的顶点
         self.dfs(1,self.flag,self.flag2)
         self.dfsPlaceHolder()   #用于恢复初始状态，使程序可以重复运行
+
+    #以greedy开头的都是和贪婪算法相关的函数
+    def greedyPlaceHolder(self):
+        self.n = 13
+        self.weight = [[0, 75, 0, 118, 0, 0, 0, 0, 0, 0, 0, 140, 0],
+                       [0, 0, 71, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 151, 0],
+                       [0, 0, 0, 0, 111, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 70, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 75, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 120, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 138, 0, 0, 0, 146],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 101, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 211, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 97, 0, 0, 80, 0]]
+
+    def Result(self,prev, t):
+        if prev[t] != 0:
+            self.Result(prev, prev[t])
+            print('->{}'.format(prev[t] + 1), end="")
+            self.dict[prev[t] + 1].setChecked(True)
+
+    def Dijkstra(self, n, weights):
+        # 创建一个flag数组，用于保存遍历情况
+        flag4 = np.zeros(n, bool)
+        # 创建一个dist数组，用于保存最短路径
+        dist = np.array(weights[0])
+        # 创建一个prev数组，用于保存对应的最短节点
+        prev = np.zeros(n, int)
+        # 将源节点放入集合S中
+        flag4[0] = True
+        # Dijkstra算法中重点：
+        # ~~错误思路：迭代(n+1)/2次，因为每次可以确定两个节点
+        # 迭代次数为n-1次，因为如果确定某一节点，但其最小值不会影响其他节点，每次迭代只能确定一个节点；
+        # 依次将节点放入集合S中（即已访问过的节点）；
+        for i in range(n - 1):
+            # 找到当前dist中还未被遍历的节点中权值最小的节点；
+            # 并将其放入集合S中；
+            temp = float('inf')
+            u = 0
+            for j in range(n):
+                if not flag4[j] and dist[j] != 0 and dist[j] < temp:
+                    u = j
+                    temp = dist[j]
+            flag4[u] = True
+            # 确定当前节点最短距离后，其他节点最短距离是否随之改变，若改变，即可确定其最短路径；
+            for j in range(n):
+                if not flag4[j] and weights[u][j] != 0:
+                    if dist[u] + weights[u][j] < dist[j] or dist[j] == 0:
+                        dist[j] = dist[u] + weights[u][j]
+                        prev[j] = u
+        # 输出结果
+        i = 9
+        print('{}：3'.format(dist[i]), end="")
+        self.dict[3].setChecked(True)
+        # 递归函数，因为prev中是从后往前读取节点
+        self.Result(prev, i)
+        print("->{}".format(i + 1))
+        self.dict[10].setChecked(True)
+
+    def greedyTest(self):
+        weights = np.array(self.weight).reshape(self.n, self.n)
+        self.Dijkstra(self.n, weights)
