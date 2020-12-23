d453d0ad442ae71b44320ef2c9f13f56d624e365
ChangWenhan <53159178+ChangWenhan@users.noreply.github.com>
2020-12-23 23:29:59 +0800

    Add files via upload

---
+++ 人工智能B/人工智能B第二次作业-遗传算法/cmake-build-debug/CMakeFiles/NQueen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NQueen.dir/main.cpp.obj"
+  "NQueen.exe"
+  "NQueen.exe.manifest"
+  "NQueen.pdb"
+  "libNQueen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NQueen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
