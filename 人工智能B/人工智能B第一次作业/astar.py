fc61a040e430c41407241522d430883843aff6f0
ChangWenhan <53159178+ChangWenhan@users.noreply.github.com>
2020-12-23 23:16:59 +0800

    Add files via upload

---
+++ 人工智能B/人工智能B第一次作业/astar.py
@@ -0,0 +1,47 @@
+straightDis = [366,0,160,242,161,176,77,151,226,244,241,234,380,100,193,253,329,80,199,374]
+placeName = ['A','B','C','D','E','F','G','H','I','L','M','N','O','P','R','S','T','U','V','Z']
+placeNum = [3,10,8,7,99,11,99,99,99,5,6,99,1,9,13,12,4,99,99,2]
+disDict = dict(zip(placeName,straightDis))
+numNameDict = dict(zip(placeName,placeNum))
+
+weight = [[1, 2,71], [2, 1,71], [1, 12,151], [12, 1,151],
+        [2, 3,75], [3, 2,75], [3, 12,140], [12, 3,140],
+        [3, 4,118], [4, 3,118], [4, 5,111], [5, 4,115],
+        [5, 6,70], [6, 5,70], [6, 7,75], [7, 6,75],
+        [7, 8,120], [8, 7,120], [8, 9,138], [9, 8,138],
+        [8, 13,146], [13, 8,146], [9, 10,101], [10, 9,101],
+        [9, 13,97], [13, 9,97], [10, 11,211], [11, 10,211],
+        [11, 12,99], [12, 11,99], [12, 13,80], [13, 12,80]]
+
+def get_key (dict, value):
+    return [k for k, v in dict.items() if v == value][0]
+
+
+path = []
+pathTest = []
+pathWeight = []
+
+def test(nodeNow,path,pathTest,pathWeight):
+    while nodeNow != 10:
+        for i in weight:
+            if i[0] == nodeNow:
+                pathTest.append(i[1])
+                pathWeight.append(i[2]+disDict[get_key(numNameDict,i[1])])
+                index = pathWeight.index(min(pathWeight))
+        path.append(pathTest[index])
+        pathTest=[]
+        pathWeight=[]
+        nodeNow = numNameDict[get_key(numNameDict,path[len(path)-1])]
+
+import datetime
+
+starttime = datetime.datetime.now()
+nodeNow = 3
+path.append(3)
+test(nodeNow,path,pathTest,pathWeight)
+endtime = datetime.datetime.now()
+k = endtime-starttime
+print(k.total_seconds())
+# print(pathTest)
+# print(pathWeight)
+
---
+++ 人工智能B/人工智能B第一次作业/dij.py
@@ -0,0 +1,74 @@
+import numpy as np
+import datetime
+
+def Result(prev, t):
+    if prev[t] != 0:
+        Result(prev, prev[t])
+        print('->{}'.format(prev[t] + 1), end="")
+
+def Dijkstra(n, weights):
+    # 创建一个flag数组，用于保存遍历情况
+    flag = np.zeros(n, bool)
+    # 创建一个dist数组，用于保存最短路径
+    dist = np.array(weights[0])
+    # 创建一个prev数组，用于保存对应的最短节点
+    prev = np.zeros(n, int)
+    # 将源节点放入集合S中
+    flag[0] = True
+    # Dijkstra算法中重点：
+    # ~~错误思路：迭代(n+1)/2次，因为每次可以确定两个节点
+    # 迭代次数为n-1次，因为如果确定某一节点，但其最小值不会影响其他节点，每次迭代只能确定一个节点；
+    # 依次将节点放入集合S中（即已访问过的节点）；
+    for i in range(n-1):
+        # 找到当前dist中还未被遍历的节点中权值最小的节点；
+        # 并将其放入集合S中；
+        temp = float('inf')
+        u = 0
+        for j in range(n):
+            if not flag[j] and dist[j] != 0 and dist[j] < temp:
+                u = j
+                temp = dist[j]
+        flag[u] = True
+        # 确定当前节点最短距离后，其他节点最短距离是否随之改变，若改变，即可确定其最短路径；
+        for j in range(n):
+            if not flag[j] and weights[u][j] != 0:
+                if dist[u] + weights[u][j] < dist[j] or dist[j] == 0:
+                    dist[j] = dist[u] + weights[u][j]
+                    prev[j] = u
+    # 输出结果
+    for i in range(1, n):
+        if i == 9:
+            print('hello world')
+        print('{}：1'.format(dist[i]), end="")
+        # 递归函数，因为prev中是从后往前读取节点
+        Result(prev, i)
+        print("->{}".format(i + 1))
+
+
+def main():
+    n = 13
+    weight = [[0,75,0,118,0,0,0,0,0,0,0,140,0],
+              [0,0,71,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,151,0],
+              [0,0,0,0,111,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,70,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,75,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,120,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,138,0,0,0,146],
+              [0,0,0,0,0,0,0,0,0,101,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0,0,0,211,0,0,0],
+              [0,0,0,0,0,0,0,0,0,0,99,0,0],
+              [0,0,0,0,0,0,0,0,97,0,0,80,0]]
+    # for i in range(n):
+    #     weight.append(list(map(int, input().rstrip().split())))
+    weights = np.array(weight).reshape(n, n)
+    starttime = datetime.datetime.now()
+    Dijkstra(n, weights)
+    endtime = datetime.datetime.now()
+    k = endtime-starttime
+    print(k.total_seconds())
+
+
+if __name__ == '__main__':
+    main()
