696a3193d1849c3e5584394b4ae2249f6e08cecb
ChangWenhan <53159178+ChangWenhan@users.noreply.github.com>
2022-04-27 15:42:51 +0800

    Add files via upload

---
+++ 毕业设计/CAFE_CIFAR_10/RESULT/PSNR.py
@@ -0,0 +1,27 @@
+import pandas as pd     #引入pandas包
+import numpy as np
+import matplotlib.pyplot as plt
+pd.set_option('display.max_rows', None)
+
+txt1 = 'VFL_cafe_cifar10_0.01.txt'
+txt2 = 'VFL_cafe_cifar10_0.01_defense.txt'
+
+table1 = pd.read_table(txt1,sep='\t',header=None)
+table2 = pd.read_table(txt2,sep='\t',header=None)     #读入txt文件，分隔符为\t
+
+print(table1)
+# table1[1][12] = 11.003674
+# table1[2][12] = 1200
+# table1[4][12] = 0.27
+
+fig, ax = plt.subplots() # 创建图实例
+
+ax.plot(table1[2], table1[1], label='Attack') 
+ax.plot(table2[2], table2[1], label='Defense')
+ax.set_xlabel('round') #设置x轴名称 x label
+ax.set_ylabel('PSNR') #设置y轴名称 y label
+ax.set_title('PSNR') 
+ax.legend() 
+
+plt.savefig("PSNR.png")
+plt.show() #图形可视化
